162e2a1112ca287cdeda4f72a9a14d21d7ed0855
PoolakuntaDakshayani03 <234g1a0533@srit.ac.in>
2025-06-27 22:45:29 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,10 +10,10 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', {
-        email,
-        password,
-      });
+      const res = await axios.post(
+        `${process.env.REACT_APP_API_BASE_URL}/auth/login`,
+        { email, password }
+      );
 
       localStorage.setItem('token', res.data.token);
       alert('Login successful');
